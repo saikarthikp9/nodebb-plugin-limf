f77836b5e40f6fb86919ad6112ccdfe7ed40df05
sai <saikarthik@saitowers.com>
2023-11-10 15:54:31 -0600

    added required limf code

--- library.js
+++ library.js
@@ -1,40 +1,158 @@
-'use strict';
+"use strict";
 
-const nconf = require.main.require('nconf');
-const winston = require.main.require('winston');
+const nconf = require.main.require("nconf");
+const winston = require.main.require("winston");
 
-const meta = require.main.require('./src/meta');
+const meta = require.main.require("./src/meta");
 
-const controllers = require('./lib/controllers');
+const controllers = require("./lib/controllers");
 
-const routeHelpers = require.main.require('./src/routes/helpers');
+const routeHelpers = require.main.require("./src/routes/helpers");
+
+const customFields = {
+  fullname: {
+    label: "Full Name",
+    placeholder: "John Smith",
+    help_text: "",
+    type: "text",
+    validation_type: "name",
+    required: true,
+    autocomplete: "name",
+  },
+  email: {
+    label: "Email Address",
+    placeholder: "john.smith@gmail.com",
+    help_text:
+      "This forum uses your email address for account recovery in the event of a lost password, as well as for scheduled digest and notifications.",
+    type: "text",
+    validation_type: "email",
+    required: true,
+    autocomplete: "email",
+  },
+  phone: {
+    label: "Phone Number",
+    placeholder: "+919848249183",
+    help_text:
+      "Include the country code, +1 for US/Canada, +91 for India, etc.",
+    type: "text",
+    validation_type: "phone",
+    required: true,
+    autocomplete: "tel",
+  },
+  current_address: {
+    label: "City of Current Residence",
+    placeholder: "Bangalore",
+    help_text:
+      "Where do you live now? We will not share this information and only use it to connect with you, if needed.",
+    type: "text",
+    validation_type: "address",
+    required: true,
+    autocomplete: "address-level2",
+  },
+  home_town: {
+    label: "Home Town",
+    placeholder: "Puttaparthi",
+    help_text:
+      "Where are you from originally? This just tells us more about your roots.",
+    type: "text",
+    validation_type: "address",
+    required: true,
+  },
+  relationship: {
+    label: "Connection to Sai Baba",
+    placeholder: "",
+    help_text:
+      "Select all that apply. We understand your background with this info. Hold down the Ctrl (Windows) or Command (Mac) button to select multiple options.",
+    type: "multiselect",
+    select_options: [
+      {
+        value: "Office Bearer Sai Organization",
+        label: "Office Bearer Sai Organization",
+      },
+      {
+        value: "Employee of Sai Institutions",
+        label: "Employee of Sai Institutions",
+      },
+      {
+        value: "Student of Sai Baba Colleges",
+        label: "Student of Sai Baba Colleges",
+      },
+      {
+        value: "Author of Sai Baba Literature",
+        label: "Author of Sai Baba Literature",
+      },
+      { value: "Child of Sai Devotees", label: "Child of Sai Devotees" },
+      { value: "Sai Devotee", label: "Sai Devotee" },
+    ],
+    required: false,
+  },
+  skills: {
+    label: "Specialist In",
+    placeholder: "",
+    help_text:
+      "Select all that apply. We may use your help based on your skills. Hold down the Ctrl (Windows) or Command (Mac) button to select multiple options.",
+    type: "multiselect",
+    select_options: [
+      { value: "Editing", label: "Editing" },
+      { value: "Writing", label: "Writing" },
+      { value: "Design/Graphics", label: "Design/Graphics" },
+      { value: "Research", label: "Research" },
+      {
+        value: "Automation of Research Process",
+        label: "Automation of Research Process",
+      },
+      { value: "Social Media Expert", label: "Social Media Expert" },
+    ],
+    required: false,
+  },
+};
+
+const user = require.main.require("./src/user");
+const db = require.main.require("./src/database");
 
 const plugin = {};
 
 plugin.init = async (params) => {
-	const { router /* , middleware , controllers */ } = params;
+  const { router /* , middleware , controllers */ } = params;
 
-	// Settings saved in the plugin settings can be retrieved via settings methods
-	const { setting1, setting2 } = await meta.settings.get('quickstart');
-	if (setting1) {
-		console.log(setting2);
-	}
+  // Settings saved in the plugin settings can be retrieved via settings methods
+  const { setting1, setting2 } = await meta.settings.get("quickstart");
+  if (setting1) {
+    console.log(setting2);
+  }
 
-	/**
-	 * We create two routes for every view. One API call, and the actual route itself.
-	 * Use the `setupPageRoute` helper and NodeBB will take care of everything for you.
-	 *
-	 * Other helpers include `setupAdminPageRoute` and `setupAPIRoute`
-	 * */
-	routeHelpers.setupPageRoute(router, '/quickstart', [(req, res, next) => {
-		winston.info(`[plugins/quickstart] In middleware. This argument can be either a single middleware or an array of middlewares`);
-		setImmediate(next);
-	}], (req, res) => {
-		winston.info(`[plugins/quickstart] Navigated to ${nconf.get('relative_path')}/quickstart`);
-		res.render('quickstart', { uid: req.uid });
-	});
+  /**
+   * We create two routes for every view. One API call, and the actual route itself.
+   * Use the `setupPageRoute` helper and NodeBB will take care of everything for you.
+   *
+   * Other helpers include `setupAdminPageRoute` and `setupAPIRoute`
+   * */
+  routeHelpers.setupPageRoute(
+    router,
+    "/quickstart",
+    [
+      (req, res, next) => {
+        winston.info(
+          `[plugins/quickstart] In middleware. This argument can be either a single middleware or an array of middlewares`
+        );
+        setImmediate(next);
+      },
+    ],
+    (req, res) => {
+      winston.info(
+        `[plugins/quickstart] Navigated to ${nconf.get(
+          "relative_path"
+        )}/quickstart`
+      );
+      res.render("quickstart", { uid: req.uid });
+    }
+  );
 
-	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/quickstart', controllers.renderAdminPage);
+  routeHelpers.setupAdminPageRoute(
+    router,
+    "/admin/plugins/quickstart",
+    controllers.renderAdminPage
+  );
 };
 
 /**
@@ -63,26 +181,215 @@ plugin.init = async (params) => {
  *	}
  */
 plugin.addRoutes = async ({ router, middleware, helpers }) => {
-	const middlewares = [
-		middleware.ensureLoggedIn,			// use this if you want only registered users to call this route
-		// middleware.admin.checkPrivileges,	// use this to restrict the route to administrators
-	];
+  const middlewares = [
+    middleware.ensureLoggedIn, // use this if you want only registered users to call this route
+    // middleware.admin.checkPrivileges,	// use this to restrict the route to administrators
+  ];
 
-	routeHelpers.setupApiRoute(router, 'get', '/quickstart/:param1', middlewares, (req, res) => {
-		helpers.formatApiResponse(200, res, {
-			foobar: req.params.param1,
-		});
-	});
+  routeHelpers.setupApiRoute(
+    router,
+    "get",
+    "/quickstart/:param1",
+    middlewares,
+    (req, res) => {
+      helpers.formatApiResponse(200, res, {
+        foobar: req.params.param1,
+      });
+    }
+  );
 };
 
 plugin.addAdminNavigation = (header) => {
-	header.plugins.push({
-		route: '/plugins/quickstart',
-		icon: 'fa-tint',
-		name: 'Quickstart',
-	});
+  header.plugins.push({
+    route: "/plugins/quickstart",
+    icon: "fa-tint",
+    name: "Quickstart",
+  });
 
-	return header;
+  return header;
+};
+
+plugin.whitelistFields = async ({ uids, whitelist }) => {
+  for (var key in customFields) {
+    whitelist.push(key);
+  }
+
+  return { uids, whitelist };
+};
+
+plugin.customHeaders = function (headers, callback) {
+  for (var key in customFields) {
+    var label = customFields[key].label;
+
+    headers.headers.push({
+      label: label,
+    });
+  }
+
+  callback(null, headers);
+};
+
+plugin.customFields = function (params, callback) {
+  var users = params.users.map(function (user) {
+    if (!user.customRows) {
+      user.customRows = [];
+      for (var key in customFields) {
+        user.customRows.push({ value: customFields[key] });
+      }
+    }
+
+    return user;
+  });
+
+  callback(null, { users: users });
+};
+
+plugin.addField = function (params, callback) {
+  for (var key in customFields) {
+    if (key == "") {
+      callback(null, params);
+      return;
+    }
+
+    var label = customFields[key].label;
+    if (customFields[key].type == "text") {
+      var html = `<input class="form-control" type="text" name="${key}" id="${key}" placeholder="${customFields[key].placeholder}" autocomplete="${customFields[key].autocomplete}"><span class="custom-feedback" id="${key}-notify"></span><span class="help-block text-xs">${customFields[key].help_text}</span>`;
+    } else if (customFields[key].type == "textarea") {
+      var html = `<textarea class="form-control" type="text" name="${key}" id="${key}" placeholder="${customFields[key].placeholder}" autocomplete="${customFields[key].autocomplete}"></textarea><span class="custom-feedback" id="${key}-notify"></span><span class="help-block text-xs">${customFields[key].help_text}</span>`;
+    } else if (customFields[key].type == "select") {
+      var html = `<select class="form-control" type="text" name="${key}" id="${key}">`;
+      for (var option of customFields[key].select_options) {
+        html += `<option value="${option.value}">${option.label}</option>`;
+      }
+      html += `</select><span class="custom-feedback" id="${key}-notify"></span><span class="help-block text-xs">${customFields[key].help_text}</span>`;
+    } else if (customFields[key].type == "checkbox") {
+      var html = `<input class="form-control" type="checkbox" name="${key}" id="${key}"><span class="custom-feedback" id="${key}-notify"></span><span class="help-block text-xs">${customFields[key].help_text}</span>`;
+    } else if (customFields[key].type == "multiselect") {
+      var html = `<select class="form-control" type="text" name="${key}" id="${key}" multiple>`;
+      for (var option of customFields[key].select_options) {
+        html += `<option value="${option.value}">${option.label}</option>`;
+      }
+      html += `</select><span class="custom-feedback" id="${key}-notify"></span><span class="help-block text-xs">${customFields[key].help_text}</span>`;
+    }
+
+    var captcha = {
+      label: label,
+      html: html,
+    };
+
+    if (
+      params.templateData.regFormEntry &&
+      Array.isArray(params.templateData.regFormEntry)
+    ) {
+      params.templateData.regFormEntry.push(captcha);
+    } else {
+      params.templateData.captcha = captcha;
+    }
+  }
+  callback(null, params);
+};
+
+plugin.checkField = function (params, callback) {
+  var userData = params.userData;
+  var error = null;
+  for (var key in customFields) {
+    var value = userData[key];
+    if (customFields[key].validation_type == "phone") {
+      if (value.length < 10 || !/^\+\d{8,}$/.test(value)) {
+        error = {
+          message:
+            "Invalid phone number. It must start with a '+' and be 10 digits or more.",
+        };
+      }
+    } else if (customFields[key].validation_type == "address") {
+      if (value.length < 3) {
+        error = { message: "Enter a valid address." };
+      } else if (/[!@$%^&*(),?":{}|<>]/.test(value)) {
+        error = {
+          message: "Invalid address. It must not contain special characters.",
+        };
+      }
+    } else if (customFields[key].validation_type == "email") {
+      if (!/^(?![A-Z])[\w\.\-\+]+@[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}$/.test(value)) {
+        error = { message: "Invalid email address." };
+      }
+    } else if (customFields[key].required) {
+      if (value == "" || value == undefined) {
+        error = {
+          message: "Please complete all required fields before submitting.",
+        };
+      }
+    }
+  }
+  callback(error, params);
+};
+
+plugin.creatingUser = function (params, callback) {
+  for (var key in customFields) {
+    params.user[key] = params.data[key];
+  }
+
+  callback(null, params);
+};
+
+plugin.createdUser = async function (params) {
+  try {
+    console.log("plugin.createdUser.params");
+    console.log("params", params);
+    var addCustomData = {};
+
+    let index = 0;
+    var keylist = [];
+    for (let key in customFields) {
+      keylist.push(key);
+      console.log(key);
+      console.log(index);
+      console.log(params.data.customRows[index]);
+      console.log(params.data.customRows[index].value);
+      console.log(typeof params.data.customRows[index].value);
+
+      if (typeof params.data.customRows[index].value == "string") {
+        addCustomData[key] = params.data.customRows[index].value;
+      } else if (typeof params.data.customRows[index].value == "object") {
+        addCustomData[key] = params.data.customRows[index].value.join(",");
+      }
+      index += 1;
+    }
+    addCustomData["uid"] = params.user.uid;
+    console.log("addCustomData", addCustomData);
+
+    var keyID = "`user:`" + params.user.uid + ":ns:custom_fields";
+
+    console.log("keyID", keyID);
+
+    await user.updateProfile(
+      // callerUid,
+      1,
+      addCustomData,
+      keylist
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+plugin.addToApprovalQueue = function (params, callback) {
+  console.log("addToApprovalQueue");
+  console.log(params.data);
+  console.log(params.userData);
+  var data = params.data;
+  var userData = params.userData;
+
+  data.customRows = [];
+
+  for (var key in customFields) {
+    var fieldData = params.userData[key];
+    console.log(fieldData);
+    console.log(typeof fieldData);
+    data.customRows.push({ value: fieldData });
+  }
+
+  console.log(data.customRows);
+  callback(null, { data: data, userData: userData });
 };
 
 module.exports = plugin;
