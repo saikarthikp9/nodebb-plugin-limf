230d2f61058163120efa7562f2fcab129fbd60cc
sai <saikarthik@saitowers.com>
2023-11-18 08:37:07 -0600

    separate form fields into multiple interstitials

--- library.js
+++ library.js
@@ -9,147 +9,164 @@ const controllers = require("./lib/controllers");
 
 const routeHelpers = require.main.require("./src/routes/helpers");
 
+// The numbers represent the interstitial index number and grouping of fields
 const customFields = {
-  fullname: {
-    label: "Full Name",
-    placeholder: "John Smith",
-    help_text: "",
-    type: "text",
-    validation_type: "name",
-    required: true,
-    autocomplete: "name",
+  0: {
+    fullname: {
+      label: "Full Name",
+      placeholder: "John Smith",
+      help_text: "",
+      type: "text",
+      validation_type: "name",
+      required: true,
+      autocomplete: "name",
+    },
+    phone: {
+      label: "Phone Number",
+      placeholder: "+919848249183",
+      help_text:
+        "Include the country code, such as +1 for the US/Canada or +91 for India, to ensure accurate contact information.",
+      type: "text",
+      validation_type: "phone",
+      required: true,
+      autocomplete: "tel",
+    },
   },
-  phone: {
-    label: "Phone Number",
-    placeholder: "+919848249183",
-    help_text:
-      "Include the country code, such as +1 for the US/Canada or +91 for India, to ensure accurate contact information.",
-    type: "text",
-    validation_type: "phone",
-    required: true,
-    autocomplete: "tel",
+  1: {
+    current_address: {
+      label: "City of Current Residence",
+      placeholder: "Bangalore",
+      help_text:
+        "Please provide your current location. We will keep this information confidential and only use it for necessary communication or interviews with your permission.",
+      type: "text",
+      validation_type: "address",
+      required: true,
+      autocomplete: "address-level2",
+    },
+    home_town: {
+      label: "Home Town",
+      placeholder: "Puttaparthi",
+      help_text:
+        "Tell us about your place of origin. This helps us learn more about your background and heritage.",
+      type: "text",
+      validation_type: "address",
+      required: true,
+    },
   },
-  current_address: {
-    label: "City of Current Residence",
-    placeholder: "Bangalore",
-    help_text:
-      "Please provide your current location. We will keep this information confidential and only use it for necessary communication or interviews with your permission.",
-    type: "text",
-    validation_type: "address",
-    required: true,
-    autocomplete: "address-level2",
-  },
-  home_town: {
-    label: "Home Town",
-    placeholder: "Puttaparthi",
-    help_text:
-      "Tell us about your place of origin. This helps us learn more about your background and heritage.",
-    type: "text",
-    validation_type: "address",
-    required: true,
-  },
-  relationship: {
-    label: "Connection to Sai Baba",
-    placeholder: "",
-    help_text:
-      "Select all that apply. This information helps us understand your background better. Use Ctrl on Windows or Command on Macs to select multiple options.",
-    type: "select",
-    select_options: [
-      {
-        value: "Office Bearer Sai Organization",
-        label: "Office Bearer Sai Organization",
-      },
-      {
-        value: "Employee of Sai Institutions",
-        label: "Employee of Sai Institutions",
-      },
-      {
-        value: "Student of Sai Baba Colleges",
-        label: "Student of Sai Baba Colleges",
-      },
-      {
-        value: "Author of Sai Baba Literature",
-        label: "Author of Sai Baba Literature",
-      },
-      { value: "Child of Sai Devotees", label: "Child of Sai Devotees" },
-      { value: "Sai Devotee", label: "Sai Devotee" },
-    ],
-    required: false,
-    multiple: true,
-  },
-  skills: {
-    label: "Specialist In",
-    placeholder: "",
-    help_text:
-      "Select all that apply. We may reach out to you based on your expertise. Use Ctrl on Windows or Command on Macs to select multiple options.",
-    type: "select",
-    select_options: [
-      { value: "Editing", label: "Editing" },
-      { value: "Writing", label: "Writing" },
-      { value: "Design/Graphics", label: "Design/Graphics" },
-      { value: "Research", label: "Research" },
-      {
-        value: "Automation of Research Process",
-        label: "Automation of Research Process",
-      },
-      { value: "Social Media Expert", label: "Social Media Expert" },
-    ],
-    required: false,
-    multiple: true,
+  2: {
+    relationship: {
+      label: "Connection to Sai Baba",
+      placeholder: "",
+      help_text:
+        "Select all that apply. This information helps us understand your background better. Use Ctrl on Windows or Command on Macs to select multiple options.",
+      type: "select",
+      select_options: [
+        {
+          value: "Office Bearer Sai Organization",
+          label: "Office Bearer Sai Organization",
+        },
+        {
+          value: "Employee of Sai Institutions",
+          label: "Employee of Sai Institutions",
+        },
+        {
+          value: "Student of Sai Baba Colleges",
+          label: "Student of Sai Baba Colleges",
+        },
+        {
+          value: "Author of Sai Baba Literature",
+          label: "Author of Sai Baba Literature",
+        },
+        { value: "Child of Sai Devotees", label: "Child of Sai Devotees" },
+        { value: "Sai Devotee", label: "Sai Devotee" },
+      ],
+      required: false,
+      multiple: true,
+    },
+    skills: {
+      label: "Specialist In",
+      placeholder: "",
+      help_text:
+        "Select all that apply. We may reach out to you based on your expertise. Use Ctrl on Windows or Command on Macs to select multiple options.",
+      type: "select",
+      select_options: [
+        { value: "Editing", label: "Editing" },
+        { value: "Writing", label: "Writing" },
+        { value: "Design/Graphics", label: "Design/Graphics" },
+        { value: "Research", label: "Research" },
+        {
+          value: "Automation of Research Process",
+          label: "Automation of Research Process",
+        },
+        { value: "Social Media Expert", label: "Social Media Expert" },
+      ],
+      required: false,
+      multiple: true,
+    },
   },
 };
 
-var fields = "";
+var formFields = [];
 
 console.log("#################### SETTINGS FIELDS ####################");
-for (var key in customFields) {
-  const label = customFields[key].label;
-  const placeholder = customFields[key].placeholder;
-  const help_text = customFields[key].help_text;
-  const type = customFields[key].type;
-  const required = customFields[key].required;
-  const autocomplete = customFields[key].autocomplete;
+for (var interstitialIndex in customFields) {
+  for (var key in customFields[interstitialIndex]) {
+    const label = customFields[interstitialIndex][key].label;
+    const placeholder = customFields[interstitialIndex][key].placeholder;
+    const help_text = customFields[interstitialIndex][key].help_text;
+    const type = customFields[interstitialIndex][key].type;
+    const required = customFields[interstitialIndex][key].required;
+    const autocomplete = customFields[interstitialIndex][key].autocomplete;
 
-  if (type == "text") {
-    fields += `
+    if (!formFields[interstitialIndex]) {
+      formFields[interstitialIndex] = "";
+    }
+    if (type == "text") {
+      formFields[interstitialIndex] += `
       <label class="form-label" for="${key}">${label}</label>
       <input class="form-control" type="${type}" id="${key}" name="${key}" placeholder="${placeholder}" autocomplete="${autocomplete}" ${
-      required ? "required" : ""
-    } />
+        required ? "required" : ""
+      } />
       <p class="form-text">${help_text}</p>
     `;
-  } else if (type == "select") {
-    const select_options = customFields[key].select_options;
-    const multiple = customFields[key].multiple;
-    var html = `<label class="form-label" for="${key}">${label}</label>
+    } else if (type == "select") {
+      const select_options =
+        customFields[interstitialIndex][key].select_options;
+      const multiple = customFields[interstitialIndex][key].multiple;
+      var html = `<label class="form-label" for="${key}">${label}</label>
     <select class="form-control" type="text" name="${key}" id="${key}" ${
-      multiple ? "multiple" : ""
-    }>`;
-    for (var option of select_options) {
-      html += `<option value="${option.value}">${option.label}</option>`;
+        multiple ? "multiple" : ""
+      }>`;
+      for (var option of select_options) {
+        html += `<option value="${option.value}">${option.label}</option>`;
+      }
+      html += `</select><p class="form-text">${help_text}</p>`;
+      formFields[interstitialIndex] += html;
     }
-    html += `</select><p class="form-text">${help_text}</p>`;
-    fields += html;
   }
 }
 
 // validates only if validation_type is set OR if required
-function validation(formData) {
+function validation(formData, interstitialIndex) {
   console.log("################ Validation");
   var error = "";
-  for (var key in customFields) {
+  for (var key in customFields[interstitialIndex]) {
     var value = formData[key];
-    if (customFields[key].validation_type == "name") {
+    if (customFields[interstitialIndex][key].validation_type == "name") {
       if (!/^([a-zA-Z ]){2,30}$/.test(value)) {
-        error += `Invalid ${customFields[key].label}. `;
+        error += `Invalid ${customFields[interstitialIndex][key].label}. `;
       }
-    } else if (customFields[key].validation_type == "phone") {
+    } else if (
+      customFields[interstitialIndex][key].validation_type == "phone"
+    ) {
       if (!/^\+([0-9]{1,3})([0-9]{6,14})$/.test(value)) {
         error += "Invalid phone number (Must start with a '+'). ";
       }
-    } else if (customFields[key].validation_type == "address") {
+    } else if (
+      customFields[interstitialIndex][key].validation_type == "address"
+    ) {
       if (!/^[a-zA-Z]{4,49}(?:[\s-'][a-zA-Z]+)*$/.test(value)) {
-        error += `Invalid ${customFields[key].label} (Include City, State). `;
+        error += `Invalid ${customFields[interstitialIndex][key].label} (Include City, State). `;
       }
     }
   }
@@ -260,21 +277,25 @@ plugin.addAdminNavigation = (header) => {
 };
 
 plugin.whitelistFields = async ({ uids, whitelist }) => {
-  for (var key in customFields) {
-    whitelist.push(key);
-    whitelist.push("custom_data_collected");
+  for (var interstitialIndex in customFields) {
+    for (var key in customFields[interstitialIndex]) {
+      whitelist.push(key);
+      whitelist.push("custom_data_collected");
+    }
   }
 
   return { uids, whitelist };
 };
 
 plugin.customHeaders = function (headers, callback) {
-  for (var key in customFields) {
-    var label = customFields[key].label;
+  for (var interstitialIndex in customFields) {
+    for (var key in customFields[interstitialIndex]) {
+      var label = customFields[interstitialIndex][key].label;
 
-    headers.headers.push({
-      label: label,
-    });
+      headers.headers.push({
+        label: label,
+      });
+    }
   }
 
   callback(null, headers);
@@ -284,8 +305,10 @@ plugin.customFields = function (params, callback) {
   var users = params.users.map(function (user) {
     if (!user.customRows) {
       user.customRows = [];
-      for (var key in customFields) {
-        user.customRows.push({ value: customFields[key] });
+      for (var interstitialIndex in customFields) {
+        for (var key in customFields[interstitialIndex]) {
+          user.customRows.push({ value: customFields[interstitialIndex][key] });
+        }
       }
     }
 
@@ -304,70 +327,79 @@ plugin.registerInterstitial = async function (data) {
     return data;
   }
 
-  // if the user already has this data saved, return early. userData contains the contents of req.session.
-  // just checking if at least the first value is entered
-  if (data.userData && data.userData.custom_data_collected) {
-    console.log(
-      "############# return",
-      "already in userData custom_data_collected"
-    );
-    return data;
-  }
-
   // if there is no user data (null case check)
   if (!data.userData) {
     console.log("############# error", "Invalid Data");
     throw new Error("Invalid Data");
   }
 
-  // if data.userData.uid is present it means this is an EXISTING user, not a new user. Check their hash to see whether they submitted the data.
-  if (data.userData.uid) {
-    const customData = await db.getObjectField(
-      `user:${data.userData.uid}`,
-      "test"
-    );
-    if (customData) {
-      console.log("############# return", "already in DB");
-      return data;
+  var customInterstitals = [];
+  // if the user already has this data saved, return early. userData contains the contents of req.session.
+  // just checking if at least the first value is entered
+  // TODO: replace with maker
+
+  for (var interstitialIndex in customFields) {
+    if (!data.userData[Object.keys(customFields[interstitialIndex])[0]]) {
+      console.log("############# ", "not in userData");
+
+      // add interstitial
+      var customInterstital = {
+        template: "partials/customRegistration",
+        data: {
+          fields: formFields[interstitialIndex],
+        },
+        // called when the form is submitted. userData is req.session, formData is the serialized form data in object format. Do value checks here and set the value in userData. It is checked at the top of this code block, remember?
+        callback: (userData, formData, next) => {
+          console.log("############# callback");
+
+          // TODO: VALIDATION
+          var error = validation(formData, interstitialIndex);
+          // var error = null;
+          // throw an error if the user didn't submit the custom data. You can pass a language key here, or just plain text. The end user will have the page reloaded and your error will be shown.
+          if (error == null) {
+            // set all values from customFields from formData to userData
+            for (var key in customFields[interstitialIndex]) {
+              userData[key] = formData[key];
+            }
+            console.log("############# userData set and then next(null)");
+            next(null);
+          } else {
+            console.log("############# error and then next(error)");
+            console.log(error);
+            next(new Error(error));
+          }
+        },
+      };
+      customInterstitals.unshift(customInterstital);
     }
   }
 
-  var customInterstital = {
-    template: "partials/customRegistration",
-    data: {
-      fields: fields,
-    },
-    // called when the form is submitted. userData is req.session, formData is the serialized form data in object format. Do value checks here and set the value in userData. It is checked at the top of this code block, remember?
-    callback: (userData, formData, next) => {
-      console.log("############# callback");
+  // I think this check is not required
+  // if data.userData.uid is present it means this is an EXISTING user, not a new user. Check their hash to see whether they submitted the data.
+  // if (data.userData.uid) {
+  //   const customData = await db.getObjectField(
+  //     `user:${data.userData.uid}`,
+  //     "test"
+  //   );
+  //   if (customData) {
+  //     console.log("############# return", "already in DB");
+  //     return data;
+  //   }
+  // }
 
-      // TODO: VALIDATION
-      var error = validation(formData);
-      // var error = null;
-      // throw an error if the user didn't submit the custom data. You can pass a language key here, or just plain text. The end user will have the page reloaded and your error will be shown.
-      if (error == null) {
-        // set all values from customFields from formData to userData
-        for (var key in customFields) {
-          userData[key] = formData[key];
-        }
-        userData["custom_data_collected"] = true;
-        console.log("############# userData set and then next(null)");
-        next(null);
-      } else {
-        console.log("############# error and then next(error)");
-        console.log(error);
-        next(new Error(error));
-      }
-    },
-  };
-  data.interstitials.unshift(customInterstital);
+  // add interstitial to data
+  for (interstitial of customInterstitals) {
+    data.interstitials.unshift(interstitial);
+  }
   console.log("############# last return");
   return data;
 };
 
 plugin.creatingUser = function (params, callback) {
-  for (var key in customFields) {
-    params.user[key] = params.data[key];
+  for (var interstitialIndex in customFields) {
+    for (var key in customFields[interstitialIndex]) {
+      params.user[key] = params.data[key];
+    }
   }
 
   callback(null, params);
@@ -380,15 +412,17 @@ plugin.createdUser = async function (params) {
 
     let index = 0;
     var keylist = [];
-    for (let key in customFields) {
-      keylist.push(key);
+    for (var interstitialIndex in customFields) {
+      for (var key in customFields[interstitialIndex]) {
+        keylist.push(key);
 
-      if (typeof params.data.customRows[index].value == "string") {
-        addCustomData[key] = params.data.customRows[index].value;
-      } else if (typeof params.data.customRows[index].value == "object") {
-        addCustomData[key] = params.data.customRows[index].value.join(",");
+        if (typeof params.data.customRows[index].value == "string") {
+          addCustomData[key] = params.data.customRows[index].value;
+        } else if (typeof params.data.customRows[index].value == "object") {
+          addCustomData[key] = params.data.customRows[index].value.join(",");
+        }
+        index += 1;
       }
-      index += 1;
     }
     addCustomData["uid"] = params.user.uid;
 
@@ -406,9 +440,11 @@ plugin.addToApprovalQueue = function (params, callback) {
 
   data.customRows = [];
 
-  for (var key in customFields) {
-    var fieldData = params.userData[key];
-    data.customRows.push({ value: fieldData });
+  for (var interstitialIndex in customFields) {
+    for (var key in customFields[interstitialIndex]) {
+      var fieldData = params.userData[key];
+      data.customRows.push({ value: fieldData });
+    }
   }
 
   callback(null, { data: data, userData: userData });
